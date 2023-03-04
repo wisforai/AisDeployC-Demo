743228d251b3006c3458f4aa85d3a7b2bd38f051
zhoujinghui <neo350942>
2023-03-05 01:19:03 +0800

    feat(version): v0.2.0

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -15,25 +15,27 @@ import base64
 import json
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
-from interface.interface import AisDeployC
+from interface.python.interface import AisDeployC
 
-def test_interface():
-    lib_path = None
-    license_path = None
+lib_path = None
+license_path = None
 
-    if "macOS" in platform.platform():
-        lib_path = "./cmake-build-debug/libAisDeployC.dylib"
-    elif "Windows" in platform.platform():
-        lib_path = "./cmake-build-release/AisDeployC.dll"
-    elif "Linux" in platform.platform():
-        lib_path = "./build/libAisDeployC.so"
+if "macOS" in platform.platform():
+    lib_path = "./cmake-build-debug/libAisDeployC.dylib"
+elif "Windows" in platform.platform():
+    lib_path = "./cmake-build-release/AisDeployC.dll"
+elif "Linux" in platform.platform():
+    lib_path = "./build/libAisDeployC.so"
 
-    if "macOS" in platform.platform():
-        license_path = "tests/assets/licenses/registed/macos_registed_info.aisl"
-    elif "Windows" in platform.platform():
-        license_path = "tests/assets/licenses/registed/windows_registed_info.aisl"
-    elif "Linux" in platform.platform():
-        license_path = "tests/assets/licenses/registed/macos_registed_info.aisl"
+if "macOS" in platform.platform():
+    license_path = "tests/assets/licenses/registed/macos_registed_info.aisl"
+elif "Windows" in platform.platform():
+    license_path = "tests/assets/licenses/registed/windows_registed_info.aisl"
+elif "Linux" in platform.platform():
+    license_path = "tests/assets/licenses/registed/linux_registed_info.aisl"
+
+def test_interface_sem():
+
 
     deploy_obj = AisDeployC(lib_path)
 
@@ -63,6 +65,40 @@ def test_interface():
     input_json["data_list"].append(file_json)
 
     ret_val = deploy_obj.process(input_json)
+
+def test_interface_det():
+
+
+    deploy_obj = AisDeployC(lib_path)
+
+    path_str = "tests/assets/models/det_setting_oen.aism"
+    gpu_id = 0
+    ret = deploy_obj.model_initialize(path_str, gpu_id)
+    assert ret == 0
+
+    ret = deploy_obj.update_license(license_path)
+
+
+    # process batch=1
+    imgPth = "tests/assets/images/0_Parade_marchingband_1_100.jpg"
+    f= open(imgPth, 'rb')
+    qrcode = base64.b64encode(f.read()).decode()
+    f.close()
+    file_json = {"type": "base64", "data": qrcode, "ch":3}
+    input_json = {"data_list": [file_json]}
+
+    ret_val = deploy_obj.process(input_json)
+
+    # process batch=2
+    imgPth = "tests/assets/images/0_Parade_marchingband_1_1004.jpeg"
+    f= open(imgPth, 'rb')
+    qrcode = base64.b64encode(f.read()).decode()
+    f.close()
+    file_json = {"type": "base64", "data": qrcode, "ch":3}
+    input_json["data_list"].append(file_json)
+
+    ret_val = deploy_obj.process(input_json)
+
 
 
 
