42413791e0996cce131e7aad22f10ace749d413b
zhoujinghui <neo350942>
2023-05-29 11:13:19 +0800

    feat(interface): update interface, changelog and documents

--- docs/html/search/all_b.js
+++ docs/html/search/all_b.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['process_0',['process',['../classinterface_1_1_ais_deploy_c.html#a80b3e6d8ec36da99678527e39302e540',1,'interface::AisDeployC']]],
-  ['process_5fimages_1',['process_images',['../interface_8h.html#a7fe9dc6212ec0772baa42cd0b012409d',1,'interface.h']]],
-  ['py_5fcompare_5fwith_5fground_5fembeddings_2',['py_compare_with_ground_embeddings',['../interface_8h.html#a8c33a77b679f7b43b534ea91727bb605',1,'interface.h']]],
-  ['py_5ffree_5fresult_3',['py_free_result',['../interface_8h.html#a62f252b1ce69d1d693c6611d4fe9c70b',1,'interface.h']]],
-  ['py_5fget_5fjson_5fstr_5fresults_4',['py_get_json_str_results',['../interface_8h.html#a22e487f1c4a49035d427ca66604ed6da',1,'interface.h']]],
-  ['py_5fprocess_5fjson_5fstr_5',['py_process_json_str',['../interface_8h.html#acc947a5e4a526f19943fcb68aedb3cce',1,'interface.h']]]
+  ['process_5fembedding_5f3d_5fprompt_1',['process_embedding_3d_prompt',['../interface_8h.html#a0da821d8333ff41caeaac1294cfdbf7b',1,'interface.h']]],
+  ['process_5fimages_2',['process_images',['../interface_8h.html#a7fe9dc6212ec0772baa42cd0b012409d',1,'interface.h']]],
+  ['py_5fcompare_5fwith_5fground_5fembeddings_3',['py_compare_with_ground_embeddings',['../interface_8h.html#a8c33a77b679f7b43b534ea91727bb605',1,'interface.h']]],
+  ['py_5ffree_5fresult_4',['py_free_result',['../interface_8h.html#a62f252b1ce69d1d693c6611d4fe9c70b',1,'interface.h']]],
+  ['py_5fget_5fjson_5fstr_5fresults_5',['py_get_json_str_results',['../interface_8h.html#a22e487f1c4a49035d427ca66604ed6da',1,'interface.h']]],
+  ['py_5fprocess_5fjson_5fstr_6',['py_process_json_str',['../interface_8h.html#acc947a5e4a526f19943fcb68aedb3cce',1,'interface.h']]]
 ];
--- docs/html/search/functions_4.js
+++ docs/html/search/functions_4.js
@@ -5,11 +5,12 @@ var searchData=
   ['get_5fcategories_2',['get_categories',['../interface_8h.html#ad910064ee799fc5edcb87daad8d4779f',1,'interface.h']]],
   ['get_5fper_5fbatch_5fclassification_3',['get_per_batch_classification',['../interface_8h.html#a1a3ad727e09ef5c0e03a606d71958aea',1,'interface.h']]],
   ['get_5fper_5fbatch_5fembeddings_4',['get_per_batch_embeddings',['../interface_8h.html#ab818838faf36e14c4077a85867c1d891',1,'interface.h']]],
-  ['get_5fper_5fbatch_5finstance_5fmask_5fareas_5',['get_per_batch_instance_mask_areas',['../interface_8h.html#a2668310c1cd829801482afa181365b0b',1,'interface.h']]],
-  ['get_5fper_5fbatch_5finstance_5fnumber_6',['get_per_batch_instance_number',['../interface_8h.html#a328fc2b206059fb5182b272a00609461',1,'interface.h']]],
-  ['get_5fper_5fbatch_5finstances_7',['get_per_batch_instances',['../interface_8h.html#ae325b89d1e477accdc51b202aa0a2047',1,'interface.h']]],
-  ['get_5fper_5fbatch_5fpose_5festimation_8',['get_per_batch_pose_estimation',['../interface_8h.html#ab9dc9c76b787d72b78ff16c88ad73553',1,'interface.h']]],
-  ['get_5fper_5fcategory_5fbinary_5fmasks_9',['get_per_category_binary_masks',['../interface_8h.html#aecd38970c14860052c0294eae27ab837',1,'interface.h']]],
-  ['get_5fper_5fcategory_5fscore_5fmaps_10',['get_per_category_score_maps',['../interface_8h.html#a0525c374545f97b789215cd6fbfe19b2',1,'interface.h']]],
-  ['get_5fversion_11',['get_version',['../interface_8h.html#a6c823c15d0437d817cbde0af6d7f4392',1,'interface.h']]]
+  ['get_5fper_5fbatch_5fembeddings_5f3d_5',['get_per_batch_embeddings_3d',['../interface_8h.html#a218346cb76f849841db09402c8643e68',1,'interface.h']]],
+  ['get_5fper_5fbatch_5finstance_5fmask_5fareas_6',['get_per_batch_instance_mask_areas',['../interface_8h.html#a2668310c1cd829801482afa181365b0b',1,'interface.h']]],
+  ['get_5fper_5fbatch_5finstance_5fnumber_7',['get_per_batch_instance_number',['../interface_8h.html#a328fc2b206059fb5182b272a00609461',1,'interface.h']]],
+  ['get_5fper_5fbatch_5finstances_8',['get_per_batch_instances',['../interface_8h.html#ae325b89d1e477accdc51b202aa0a2047',1,'interface.h']]],
+  ['get_5fper_5fbatch_5fpose_5festimation_9',['get_per_batch_pose_estimation',['../interface_8h.html#ab9dc9c76b787d72b78ff16c88ad73553',1,'interface.h']]],
+  ['get_5fper_5fcategory_5fbinary_5fmasks_10',['get_per_category_binary_masks',['../interface_8h.html#aecd38970c14860052c0294eae27ab837',1,'interface.h']]],
+  ['get_5fper_5fcategory_5fscore_5fmaps_11',['get_per_category_score_maps',['../interface_8h.html#a0525c374545f97b789215cd6fbfe19b2',1,'interface.h']]],
+  ['get_5fversion_12',['get_version',['../interface_8h.html#a6c823c15d0437d817cbde0af6d7f4392',1,'interface.h']]]
 ];
